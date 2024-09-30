5e091c73c4ed285e3c22120a263802af5336d136
22hr1a0521 <saipriyac2021@gmail.com>
2024-09-30 11:28:02 +0530

    Create decision.py

---
+++ decision.py
@@ -0,0 +1,109 @@
+Input format:
+Input consist of 2 integers
+The first input corresponds to  the first number.(a)
+The second input corresponds to the second number.(b)
+Output format:
+If the first number is equal to the second number, print "x and y are equal". Otherwise, print "x greater than y" or "x less than y"
+Sample Input:
+6
+8
+Sample Output:
+6 less than 8
+Input format:
+The input consist of a character
+Output format:
+The output consists of a below-given string
+“Vowel” / “Consonant” / “Not an alphabet”
+Sample Input:
+e
+
+Sample Output:
+Vowel
+Input format:
+The input consists of one integer which corresponds to the marks scored by the student
+
+Output format:
+If a student marks greater than 100, print "Invalid Input". Otherwise, print the grade.
+Sample Input:
+78
+Sample Output:C
+Input format:
+Input consists of 2 floating point numbers
+The first input corresponds to the total cost(X)
+The second input corresponds to the sold cost(Y)
+Output format:
+Print "Profit or Loss" with Rupees.
+Sample Input:
+60
+4
+Sample Output:
+Loss : Rs.12.00
+Input format:
+The first input corresponds to the student type
+The second input corresponds to the tuition fee
+The third input corresponds to the bus fee or hostel fees
+Output format:
+Print the total fee amount of the corresponding student with 2 decimal places.
+Refer below sample output for formatting
+Sample Input:
+MSH
+40000
+50000
+Sample Output:
+90000.00
+Input format:
+Input consists of 2 integers
+The first integer corresponds to the last 2 digits of the birth year
+The second integer corresponds to the last 2 digits of the current year
+Output format:
+Print the user's current age
+Refer below sample output for formatting.
+Sample Input:
+62
+00
+Sample Output:
+38
+Input format:
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+Output format:
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
+Input format:
+Input consists of 3 integers and a string
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
+Output format:
+Print the minimal seating capacity lab amongst the available labs.
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
+Input format:
+Input consists of 4 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the number of students(x)
+Output format:
+Print the number of labs which can accommodate the 'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25

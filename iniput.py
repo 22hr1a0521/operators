b8f3de0777870ebdfdcc324d9f50eb086234560b
22hr1a0521 <saipriyac2021@gmail.com>
2024-09-30 11:07:41 +0530

    Create iniput.py

---
+++ iniput.py
@@ -0,0 +1,45 @@
+
+Input is an integer that corresponds to the age of Dhoni.
+Output Format: 
+Display the age.
+Refer sample Input and Output for formatting specifications.
+[All the text in bold corresponds to the Input]
+Sample Input and Output:
+Enter the age:
+35
+Age of Dhoni is 35
+Input and Output Format:
+Refer Sample input and output for formatting specifications.
+All text in bold corresponds to input and the rest corresponds to output.
+Sample Input and Output:
+Banerjee's Question:
+Will you be a wicketkeeper?
+Dhoni's Reply:
+I will if i get a chance.
+For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply was "I will if I get a chance."
+Refer sample input and output for formatting specifications.
+[All text in bold corresponds to input and the rest corresponds to output]
+Sample Input and Output:
+Team 1:
+Team Name:
+DAV Jawahar Vidhya Mandir
+Score:
+150
+Overs played:
+20
+Team 2:
+Team name:
+Kendriya School
+Score:
+110
+Overs played:
+18
+Match Details:
+Team 1:
+Name: DAV Jawahar Vidhya Mandir
+Score: 150
+Overs played: 20
+Team 2:
+Name:  Kendriya School
+Score: 110
+Overs played: 18

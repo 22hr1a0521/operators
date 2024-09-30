a2468f8741fb455cfc965cf7d1aa824bae892044
22hr1a0521 <saipriyac2021@gmail.com>
2024-09-30 11:29:34 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,27 @@
+Input format:
+The first input containing an integer which denotes the number of the month
+Output format:
+Print the amoeba size.
+Refer the sample output for formatting.
+Sample Input:
+7
+Sample Output:
+8
+Input format:
+The input containing an integer 'n' which denotes the given number.
+Output format:
+If the given number is factorial, print "Yes". Otherwise, print "No".
+Refer the sample output for formatting.
+Sample Input:
+6
+Sample Output:
+Yes
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+Print the  'n' terms of the Lucas Sequence, separated by a single space. There are no leading or trailing spaces in the output.
+Refer the sample output for formatting.
+Sample Input:
+5
+Sample Output:
+0 0 1 1 2
